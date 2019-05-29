2c7ea6ac82c1673a531d7167d6006033b38d6a2b
Moroni Angelo <angelo.moroni@hype.it>
2019-05-29 17:36:11 +0200

    info slides

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     name: `White Label`,
-    title: `Titolo`,
-    date: `06 Aprile 2019`,
-    event: `console.log("Hello World");`,
+    title: `La creatività dello sviluppatore`,
+    date: `01 Giugno 2019`,
+    event: `Learn, code, design`,
     logo: `./logo.jpg`,
     sponsor: `./sponsor.jpg`,
   },
