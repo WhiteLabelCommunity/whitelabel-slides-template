dafeba15e8234f4c2fa051b12e93c4707e50323d
Stefano Rosso <S.Rosso.1993@gmail.com>
2019-11-16 07:35:47 +0100

    Presentation commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     name: `White Label`,
-    title: `Titolo`,
-    date: `06 Aprile 2019`,
-    event: `console.log("Hello World");`,
+    title: `The great Gatsby.js`,
+    date: `16 Novembre 2019`,
+    event: `Headless is the new black`,
     logo: `./logo.jpg`,
     sponsor: `./sponsor.jpg`,
   },
